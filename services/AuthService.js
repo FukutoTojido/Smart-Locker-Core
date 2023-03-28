578f4925f60462167339e4a65bee4b48ea8b2b5d
nguyendhst <mountngubinh@gmail.com>
2023-03-28 22:00:23 +0700

    feat: auth service

---
+++ services/AuthService.js
@@ -0,0 +1,82 @@
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const server = 'https://smart-locker-backend-hcttee6c3a-uc.a.run.app';
+
+// TEST ACCOUNT
+//{
+//    "email": "test3@gmail.com",
+//    "password": "123455688"
+//}
+
+const signIn = async (email, password) => {
+	try {
+		const response = await fetch(`${server}/api/users/login`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		});
+		const json = await response.json();
+		if (json.token) {
+			await AsyncStorage.setItem('userToken', json.token);
+		}
+		return json;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const signOut = async () => {
+	try {
+		await AsyncStorage.removeItem('userToken');
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const getToken = async () => {
+	try {
+		const token = await AsyncStorage.getItem('userToken');
+		return token;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const register = async (email, password) => {
+	try {
+		const response = await fetch(`${server}/api/users/register`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		});
+		const json = await response.json();
+		if (json.token) {
+			await AsyncStorage.setItem('userToken', json.token);
+		}
+		return json;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+
+export default {
+	signIn,
+	signOut,
+	getToken,
+	register,
+};
+
